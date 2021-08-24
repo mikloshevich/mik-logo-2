3e91453cbf7554d19bcbe87fe69a8c4651c65229
mikloshevich <pavkatc@gmail.com>
2021-08-24 18:32:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const timeline = gsap.timeline({defaults: {duration: 1}});
 const timeline2 = gsap.timeline({defaults: {duration: 1}});
 
-timeline.delay(2)
+timeline.delay(1)
 timeline
     // .from('.letters', {duration: 0.5, x: '300px'})
     .from('.letters', {duration: 1.5, scale: 0, transformOrigin: 'center'})
@@ -9,6 +9,6 @@ timeline
     .from('.letters', {duration: 2.5, rotation: 360, transformOrigin: 'center', ease: "expo.out"}, "-=1.5")
     .from('.dot', {duration: 2, rotation: 720, x: '-300px', y: '-200px'}, "-=2.5")
 
-    timeline2.delay(4.5)
+    timeline2.delay(3.5)
     timeline2
         .from('.logo', {duration: 3, scale: 0, transformOrigin: 'center', ease: 'elastic'})
