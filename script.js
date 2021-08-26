9d0686d7e52fdad7b41dfc27e99027f75635e366
mikloshevich <pavkatc@gmail.com>
2021-08-26 19:19:06 +0300

    minor changes

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let body = document.querySelector('body');
+// console.log(body);
+window.addEventListener('load', function () {
+    body.style.opacity = 1
+})
+
 const timeline = gsap.timeline({defaults: {duration: 1}});
 const timeline2 = gsap.timeline({defaults: {duration: 1}});
 
@@ -8,6 +14,8 @@ timeline
     .from('.letters', {duration: 1.5, transformOrigin: 'center'}, "-=1.5")
     .from('.letters', {duration: 2.5, rotation: 360, transformOrigin: 'center', ease: "expo.out"}, "-=1.5")
     .from('.dot', {duration: 2, rotation: 720, x: '-300px', y: '-200px'}, "-=2.5")
+    .to('.dot', {duration: 2, /*fill:"hsl(4, 81%, 59%)"*/}, "-=1.8")
+    .from('.dot', {duration: 0.5, opacity: 0}, "-=1.8")
 
     timeline2.delay(3.5)
     timeline2
